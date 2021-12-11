d89ec02eccd56bc93db34a83db83aad35caee3e4
khadetou <khadetou96@gmail.com>
2021-12-11 22:17:46 +0000

    home page

---
+++ lib/constanst.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+Color primaryTextColor = const Color(0xFF414C6B);
+Color secondaryTextColor = const Color(0xFFE4979E);
+Color primaryColor = Colors.white;
+Color titleTextColor = const Color(0xFF868686);
+Color contentTextColor = const Color(0xFF6751B5);
+Color gradientStartColor = const Color(0xFF0050AC);
+Color gradientEndColor = const Color(0xFF9354b9);
---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import "package:flutter/material.dart";
+
+import 'constanst.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: gradientEndColor,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [gradientStartColor, gradientEndColor],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: const [0.3, 0.7],
+          ),
+        ),
+      ),
+    );
+  }
+}
