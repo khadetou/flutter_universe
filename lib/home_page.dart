caa2d1c68bb2bd6e0b7310309a5364c0a809d876
khadetou <khadetou96@gmail.com>
2021-12-11 22:46:09 +0000

    homepage desing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -23,6 +23,51 @@ class _HomePageState extends State<HomePage> {
             stops: const [0.3, 0.7],
           ),
         ),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Column(
+                  children: <Widget>[
+                    const Text(
+                      "Explore",
+                      style: TextStyle(
+                        fontFamily: "Avenir",
+                        fontSize: 44,
+                        color: Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.w900,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                    DropdownButton(
+                      items: const [
+                        DropdownMenuItem(
+                          child: Text(
+                            "Solar System",
+                            style: TextStyle(
+                                fontFamily: "Avenir",
+                                fontSize: 24,
+                                color: Color(0x7cdbf1ff),
+                                fontWeight: FontWeight.w500),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ],
+                      onChanged: null,
+                      icon: Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Image.asset('assets/drop_down_icon.png'),
+                      ),
+                      underline: const SizedBox(),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
